c81c15bc5b8bcba51e29fafe0f5b492bf9d1551a
cristianopgb <cristiano@fintegra.com.br>
2026-08-06 10:03:10 -0300

    hotfix: disable occurrences module to restore api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -22,10 +22,9 @@ import { UsersController } from './users/users.controller';
 import { ReportsModule } from './reports/reports.module';
 import { AgentsModule } from './agents/agents.module';
 import { ApiIntegrationsModule } from './api-integrations/api-integrations.module';
-import { OccurrencesModule } from './occurrences/occurrences.module';
 
 @Module({
-  imports: [SupabaseModule, AuthModule, RbacModule, PlansModule, SubscriptionsModule, UsageModule, SetupModule, DataContractsModule, StagingModule, CanonicalModule, NormalizationModule, NativeRecordsModule, NativeIndicatorsModule, CustomIndicatorsModule, DashboardsModule, ReportsModule, AgentsModule, ApiIntegrationsModule, OccurrencesModule],
+  imports: [SupabaseModule, AuthModule, RbacModule, PlansModule, SubscriptionsModule, UsageModule, SetupModule, DataContractsModule, StagingModule, CanonicalModule, NormalizationModule, NativeRecordsModule, NativeIndicatorsModule, CustomIndicatorsModule, DashboardsModule, ReportsModule, AgentsModule, ApiIntegrationsModule],
   controllers: [HealthController, UsersController, TenantsController, ModulesController, RbacController],
 })
 export class AppModule {}
